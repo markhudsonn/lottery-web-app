c621eb8df14e7540c51e7d49f03d9dc466802bc5
markhudsonn <markhudsonn4@gmail.com>
2023-10-17 11:37:52 +0100

    Added phone number validation

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired, Email, email_validator
-from users.validation import name_special_character_check
+from users.validation import name_special_character_check, phone_number_check
 
 
 class RegisterForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired(), Email()])
     firstname = StringField('First Name', validators=[DataRequired(), name_special_character_check])
     lastname = StringField('Last Name', validators=[DataRequired(), name_special_character_check])
-    phone = StringField('Phone Number', validators=[DataRequired()])
+    phone = StringField('Phone Number', validators=[DataRequired(), phone_number_check])
     password = PasswordField()
     confirm_password = PasswordField()
     submit = SubmitField()
--- users/validation.py
+++ users/validation.py
@@ -3,5 +3,12 @@ import re
 
 
 def name_special_character_check(form, field):
+    """Checks if first or last name contain any special characters"""
     if re.search(r'[*?!\'^+%&/()=}\][{$#@<>]', field.data):
-        raise ValidationError('Name must not contain any of: *?!\'^+%&/()=}\][{$#@<>')
+        raise ValidationError("Name must not contain any of: *?!\'^+%&/()=}\][{$#@<>")
+
+
+def phone_number_check(form, field):
+    """"Checks if phone number in correct format"""
+    if not re.search(r'^\d{4}-\d{3}-\d{4}$', field.data):
+        raise ValidationError("Phone number must be in format: XXXX-XXX-XXXX")
