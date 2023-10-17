9f3c64eb54a48da2aad67708a647e5a8539e20b4
markhudsonn <markhudsonn4@gmail.com>
2023-10-17 11:55:52 +0100

    Added password validation

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, Email, email_validator
-from users.validation import name_special_character_check, phone_number_check
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+from users.validation import *
 
 
 class RegisterForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired(), Email()])
-    firstname = StringField('First Name', validators=[DataRequired(), name_special_character_check])
-    lastname = StringField('Last Name', validators=[DataRequired(), name_special_character_check])
-    phone = StringField('Phone Number', validators=[DataRequired(), phone_number_check])
-    password = PasswordField()
-    confirm_password = PasswordField()
+    firstname = StringField('First Name', validators=[DataRequired(), name_special_character_validation])
+    lastname = StringField('Last Name', validators=[DataRequired(), name_special_character_validation])
+    phone = StringField('Phone Number', validators=[DataRequired(), phone_number_validation])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=12), password_digit_validation,
+                                                     password_lowercase_validation, password_uppercase_validation,
+                                                     password_special_character_validation])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField()
--- users/validation.py
+++ users/validation.py
@@ -1,14 +1,39 @@
-from wtforms import ValidationError
 import re
 
+from wtforms import ValidationError
 
-def name_special_character_check(form, field):
+
+def name_special_character_validation(form, field):
     """Checks if first or last name contain any special characters"""
     if re.search(r'[*?!\'^+%&/()=}\][{$#@<>]', field.data):
         raise ValidationError("Name must not contain any of: *?!\'^+%&/()=}\][{$#@<>")
 
 
-def phone_number_check(form, field):
+def phone_number_validation(form, field):
     """"Checks if phone number in correct format"""
     if not re.search(r'^\d{4}-\d{3}-\d{4}$', field.data):
         raise ValidationError("Phone number must be in format: XXXX-XXX-XXXX")
+
+
+def password_digit_validation(form, field):
+    """Checks password has at least 1 digit"""
+    if not re.search(r'(?=.*\d)', field.data):
+        raise ValidationError("Password must contain at least 1 digit")
+
+
+def password_lowercase_validation(form, field):
+    """Checks password has at least 1 lowercase letter"""
+    if not re.search(r'(?=.*[a-z])', field.data):
+        raise ValidationError("Password must contain at least 1 lowercase letter")
+
+
+def password_uppercase_validation(form, field):
+    """Checks password has at least 1 uppercase letter"""
+    if not re.search(r'(?=.*[A-Z])', field.data):
+        raise ValidationError("Password must contain at least 1 uppercase letter")
+
+
+def password_special_character_validation(form, field):
+    """Checks password has at least 1 special character"""
+    if not re.search(r'(?=.*[*?!\'^+%&/()=}\][{$#@<>])', field.data):
+        raise ValidationError("Password must contain at least 1 special character")
