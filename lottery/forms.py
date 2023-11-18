08df0cc264d1196d22ce881f2ab089673c4ded56
markhudsonn <markhudsonn4@gmail.com>
2023-11-18 23:40:26 +0000

    Added unique draw numbers check

--- lottery/forms.py
+++ lottery/forms.py
@@ -1,14 +1,27 @@
+from flask import flash
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
 
 class DrawForm(FlaskForm):
-    # number between 1 and 60
     number1 = IntegerField(id='no1', validators=[DataRequired(), NumberRange(min=1, max=60)])
     number2 = IntegerField(id='no2', validators=[DataRequired(), NumberRange(min=1, max=60)])
     number3 = IntegerField(id='no3', validators=[DataRequired(), NumberRange(min=1, max=60)])
     number4 = IntegerField(id='no4', validators=[DataRequired(), NumberRange(min=1, max=60)])
     number5 = IntegerField(id='no5', validators=[DataRequired(), NumberRange(min=1, max=60)])
     number6 = IntegerField(id='no6', validators=[DataRequired(), NumberRange(min=1, max=60)])
+
     submit = SubmitField("Submit Draw")
+
+    def validate(self, **kwargs):
+        standard_validators = FlaskForm.validate(self)
+        if standard_validators:
+            nums = [self.number1.data, self.number2.data, self.number3.data,
+                    self.number4.data, self.number5.data, self.number6.data]
+
+            if len(nums) != len(set(nums)):  # Set removes duplicates in nums
+                flash('You cannot have duplicate numbers')
+                return False
+            return True
+        return False
